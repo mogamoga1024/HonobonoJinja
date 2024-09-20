d307ed166812bdd6ce0d6e787454e820f0bb2a3b
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-20 09:47:32 +0900

    ダブクオ派なんだよね

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let synth2 = null;
 
 Tone.Transport.bpm.value = 76;
 
-const t8n = Tone.Time('8n').toSeconds();
+const t8n = Tone.Time("8n").toSeconds();
 const t4n = t8n*2;
 
 let timer1 = 0;
@@ -13,14 +13,14 @@ function playMelody1() {
     let time = Tone.now();
     const startTime = time;
 
-    synth1.triggerAttackRelease('C5', '4n', time); time += t4n;
-    synth1.triggerAttackRelease('G5', '8n', time); time += t4n;
-    synth1.triggerAttackRelease('B4', '4n', time); time += t4n;
-    synth1.triggerAttackRelease('G5', '8n', time); time += t4n;
-    synth1.triggerAttackRelease('A4', '4n', time); time += t4n;
-    synth1.triggerAttackRelease('F5', '8n', time); time += t4n;
-    synth1.triggerAttackRelease('B4', '4n', time); time += t4n;
-    synth1.triggerAttackRelease('G5', '8n', time); time += t4n;
+    synth1.triggerAttackRelease("C5", "4n", time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
+    synth1.triggerAttackRelease("B4", "4n", time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
+    synth1.triggerAttackRelease("A4", "4n", time); time += t4n;
+    synth1.triggerAttackRelease("F5", "8n", time); time += t4n;
+    synth1.triggerAttackRelease("B4", "4n", time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
 
     timer1 = setTimeout(playMelody2, (time - startTime) * 1000);
 }
@@ -29,128 +29,129 @@ function playMelody2() {
     let time = Tone.now();
     const startTime = time;
     
-    synth1.triggerAttackRelease('G5', '4n', time);
-    synth2.triggerAttackRelease('C3', '4n', time); time += t4n; 
+    synth1.triggerAttackRelease("G5", "4n", time);
+    synth2.triggerAttackRelease("C3", "4n", time); time += t4n; 
 
-    synth1.triggerAttackRelease('G5', '8n', time + t8n);
-    synth2.triggerAttackRelease('C3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time + t8n);
+    synth2.triggerAttackRelease("C3", "4n", time); time += t4n;
 
-    synth2.triggerAttackRelease('F2', '8n', time); time += t8n;
+    synth2.triggerAttackRelease("F2", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('F5', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("F5", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('F5', '8n', time);
-    synth1.triggerAttackRelease('A5', '8n', time + t8n);
-    synth2.triggerAttackRelease('F3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("F5", "8n", time);
+    synth1.triggerAttackRelease("A5", "8n", time + t8n);
+    synth2.triggerAttackRelease("F3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '4n', time);
-    synth2.triggerAttackRelease('C3', '8n', time);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("G5", "4n", time);
+    synth2.triggerAttackRelease("C3", "8n", time);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('C5', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("C5", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('F5', '8n', time);
-    synth2.triggerAttackRelease('F2', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("F5", "8n", time);
+    synth2.triggerAttackRelease("F2", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('E5', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("E5", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('D5', '8n', time);
-    synth1.triggerAttackRelease('E5', '8n', time + t8n);
-    synth2.triggerAttackRelease('F3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("D5", "8n", time);
+    synth1.triggerAttackRelease("E5", "8n", time + t8n);
+    synth2.triggerAttackRelease("F3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('C5', '4n', time);
-    synth2.triggerAttackRelease('C3', '8n', time);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("C5", "4n", time);
+    synth2.triggerAttackRelease("C3", "8n", time);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth2.triggerAttackRelease('G2', '8n', time); time += t8n;
+    synth2.triggerAttackRelease("G2", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('F5', '8n', time);
-    synth2.triggerAttackRelease('D3', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("F5", "8n", time);
+    synth2.triggerAttackRelease("D3", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('B5', '8n', time);
-    synth1.triggerAttackRelease('C6', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("B5", "8n", time);
+    synth1.triggerAttackRelease("C6", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '2n', time);
-    synth2.triggerAttackRelease('E3', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time + t8n);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n*2);
-    synth2.triggerAttackRelease('G3', '8n', time + t8n*3); time += t4n*2;
+    synth1.triggerAttackRelease("G5", "2n", time);
+    synth2.triggerAttackRelease("E3", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time + t8n);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n*2);
+    synth2.triggerAttackRelease("G3", "8n", time + t8n*3); time += t4n*2;
 
-    synth2.triggerAttackRelease('E3', '8n', time); time += t4n;
+    synth2.triggerAttackRelease("E3", "8n", time); time += t4n;
 
-    synth2.triggerAttackRelease('D3', '8n', time); time += t4n;
+    synth2.triggerAttackRelease("D3", "8n", time); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '4n', time);
-    synth2.triggerAttackRelease('C3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("G5", "4n", time);
+    synth2.triggerAttackRelease("C3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '8n', time + t8n);
-    synth2.triggerAttackRelease('C3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("G5", "8n", time + t8n);
+    synth2.triggerAttackRelease("C3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('F5', '8n', time + t8n);
-    synth2.triggerAttackRelease('A2', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("F5", "8n", time + t8n);
+    synth2.triggerAttackRelease("A2", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('F5', '8n', time);
-    synth1.triggerAttackRelease('A5', '8n', time + t8n);
-    synth2.triggerAttackRelease('F3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("F5", "8n", time);
+    synth1.triggerAttackRelease("A5", "8n", time + t8n);
+    synth2.triggerAttackRelease("F3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('G5', '4n', time);
-    synth2.triggerAttackRelease('C3', '8n', time);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("G5", "4n", time);
+    synth2.triggerAttackRelease("C3", "8n", time);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('C6', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("C6", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('C6', '8n', time);
-    synth2.triggerAttackRelease('A2', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("C6", "8n", time);
+    synth2.triggerAttackRelease("A2", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('C6', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time); time += t8n;
+    synth1.triggerAttackRelease("C6", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time); time += t8n;
 
-    synth1.triggerAttackRelease('D6', '8n', time);
-    synth1.triggerAttackRelease('E6', '8n', time + t8n);
-    synth2.triggerAttackRelease('F3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("D6", "8n", time);
+    synth1.triggerAttackRelease("E6", "8n", time + t8n);
+    synth2.triggerAttackRelease("F3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('C6', '4n', time);
-    synth2.triggerAttackRelease('C3', '8n', time);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("C6", "4n", time);
+    synth2.triggerAttackRelease("C3", "8n", time);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('G6', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("G6", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('F6', '8n', time + t8n);
-    synth2.triggerAttackRelease('B2', '8n', time);
-    synth2.triggerAttackRelease('D3', '8n', time + t8n); time += t4n;
+    synth1.triggerAttackRelease("F6", "8n", time + t8n);
+    synth2.triggerAttackRelease("B2", "8n", time);
+    synth2.triggerAttackRelease("D3", "8n", time + t8n); time += t4n;
 
-    synth1.triggerAttackRelease('D6', '8n', time);
-    synth1.triggerAttackRelease('B5', '8n', time + t8n);
-    synth2.triggerAttackRelease('G3', '4n', time); time += t4n;
+    synth1.triggerAttackRelease("D6", "8n", time);
+    synth1.triggerAttackRelease("B5", "8n", time + t8n);
+    synth2.triggerAttackRelease("G3", "4n", time); time += t4n;
 
-    synth1.triggerAttackRelease('C6', '2n', time);
-    synth2.triggerAttackRelease('E3', '8n', time);
-    synth2.triggerAttackRelease('C3', '8n', time + t8n);
-    synth2.triggerAttackRelease('E3', '8n', time + t8n*2);
-    synth2.triggerAttackRelease('G3', '8n', time + t8n*3);
-    synth2.triggerAttackRelease('C4', '4n', time + t8n*4); time += t8n*8;
+    synth1.triggerAttackRelease("C6", "2n", time);
+    synth2.triggerAttackRelease("E3", "8n", time);
+    synth2.triggerAttackRelease("C3", "8n", time + t8n);
+    synth2.triggerAttackRelease("E3", "8n", time + t8n*2);
+    synth2.triggerAttackRelease("G3", "8n", time + t8n*3);
+    synth2.triggerAttackRelease("C4", "4n", time + t8n*4); time += t8n*8;
 
     timer2 = setTimeout(playMelody2, (time - startTime) * 1000);
 }
 
-document.getElementById('play').addEventListener('click', async () => {
+document.getElementById("play").addEventListener("click", async () => {
     await Tone.start();
-    synth1 = new Tone.Synth().toDestination();
-    synth2 = new Tone.Synth().toDestination();
+    const options = {volume: -3};
+    synth1 = new Tone.Synth(options).toDestination();
+    synth2 = new Tone.Synth(options).toDestination();
     playMelody1();
 });
 
-document.getElementById('stop').addEventListener('click', () => {
+document.getElementById("stop").addEventListener("click", () => {
     clearTimeout(timer1);
     clearTimeout(timer2);
     synth1.triggerRelease();
